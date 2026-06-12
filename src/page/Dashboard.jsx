17a880f0e13a05c787cdb380408b47271a2c89f1
Adimas Bayu <adimasb0802@gmail.com>
2026-06-12 16:23:55 +0700

    update connect-mqtt

--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -1,10 +1,46 @@
 import "./Dashboard.css";
 import TemperatureCard from "../component/TempartureCard/TemperatureCard";
 import StatusCard from "../component/StatusCard/StatusCard";
-import { dailySummary, dataTemp } from "../assets/data/DummyData";
+import { dailySummary } from "../assets/data/DummyData";
 import ChartBar from "../component/ChartBar/ChartBar";
+import { useState, useEffect } from "react";
+import client from "../services/mqttServices";
 
 export default function Dashboard() {
+  useEffect(() => {
+    client.on("connect", () => {
+      console.log("Connected");
+
+      client.subscribe("iot-monitoring-kelompok/suhu", (err) => {
+        if (!err) {
+          console.log("Subscribed");
+        }
+      });
+    });
+
+    client.on("message", (topic, message) => {
+      const data = JSON.parse(message.toString());
+
+      setdataTemp({
+        temperature: data.temperature,
+        humidity: data.humidity,
+        status: data.temperature > 35 ? "Overheat" : "Normal",
+        lastUpdated: new Date().toLocaleTimeString(),
+      });
+    });
+
+    return () => {
+      client.removeAllListeners("message");
+    };
+  }, []);
+
+  const [dataTemp, setdataTemp] = useState({
+    temperature: 0,
+    humidity: 0,
+    status: "Normal",
+    lastUpdated: "-",
+  });
+
   const hour = new Date().getHours();
   let greeting = "Selamat Pagi";
 
